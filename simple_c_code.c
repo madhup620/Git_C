1637d29727c1429bd43a9397b5ded0de2ad33662
madhup620 <53480702+madhup620@users.noreply.github.com>
2021-05-04 22:54:47 +1200

    Updated code to create static analysis warning

--- simple_c_code.c
+++ simple_c_code.c
@@ -1,5 +1,8 @@
 main() {
+  int i;
+  
   printf("This is a C example code in github\n");
   printf("This is an additional line for making a new revision of code\n");
   printf("This is the third line added from git clone in workspace via terminal");
+  i = i+10;
 }
