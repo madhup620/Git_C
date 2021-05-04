80df48a27bf73b7d7d660de278fad52b7434fa07
madhup620 <53480702+madhup620@users.noreply.github.com>
2021-05-04 23:06:34 +1200

    Update simple_c_code.c

--- simple_c_code.c
+++ simple_c_code.c
@@ -4,5 +4,5 @@ main() {
   printf("This is a C example code in github\n");
   printf("This is an additional line for making a new revision of code\n");
   printf("This is the third line added from git clone in workspace via terminal");
-  i = i+10;
+  i = i+10
 }
